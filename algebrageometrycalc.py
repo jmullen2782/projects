b6db8c61362ba712cd1c2d033dd485ea8156cead
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-10-17 10:43:11 -0400

    Add files via upload

--- algebrageometrycalc.py
+++ algebrageometrycalc.py
@@ -1,7 +1,7 @@
 '''
-Created on Oct 15, 2018
+Created on Oct 17, 2018
 
-@author: jmullen19
+@author: jenniemullen
 '''
 name=input('Please enter your name:')
 print('Hello,', name,',welcome to the Algebra and geometry calculator')
@@ -10,16 +10,17 @@ print('\n')
 print('Area of the Triangle:')
 baseT=float(input('Please enter value of base of triangle:'))
 heightT=float(input('Please enter value of height of triangle:'))
+unitT=input('Please enter your unit of measurement:')
 areaT=(baseT*heightT)/2
 num=areaT
-print('The area of the triangle is', format(num,'6.2f'), 'cm^2')
+print('The area of the triangle is', format(num,'6.2f'), unitT)
 input('Press Enter to continue')
 print('\n')
 print('Volume of a Cube:')
 sideC=float(input('Please enter value of side of cube:'))
+unitC=input('Please enter your unit of measurement:')
 volumeC=sideC**3
 num=volumeC
-print('The area of the triangle is', format(num,'8.2f'), 'cm^3')
+print('The area of the triangle is', format(num,'8.2f'), unitC)
 print('\n')
 print('Goodbye,', name, ',thank you for using this calculator')
-
