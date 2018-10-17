8c55108bab25468a37b61aa0099f80a7ffaee1ac
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-10-17 12:45:49 -0400

    Add files via upload

---
+++ accountformat.py
@@ -0,0 +1,21 @@
+'''
+Created on Oct 17, 2018
+
+@author: jmullen19
+'''
+name1=input("Please enter the first account holder's name:")
+accountnum1=input('Please enter the first account number:')
+accounttype1=input('Please enter the type of account:')
+balance1=input('Please enter the balance:')
+name2=input("Please enter the second account holder's name:")
+accountnum2=input('Please enter the second account number:')
+accounttype2=input('Please enter the type of account:')
+balance2=input('Please enter the balance:')
+name3=input("Please enter the third account holder's name:")
+accountnum3=input('Please enter the third account number:')
+accounttype3=input('Please enter the type of account:')
+balance3=input('Please enter the balance:')
+
+print('All Accounts:')
+print('Acc Num:', format(accountnum1,'>011s'), 'Name:', format(name1,'>11s'), 'Account:', )
+print()
