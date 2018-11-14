3f6c5ae1bd37cd3268ca07f7e13904cf9e0690d2
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-11-14 12:44:47 -0500

    Add files via upload

---
+++ stuff.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 14, 2018
+
+@author: Jennie Mullen
+'''
+import random
+randnum= random.randrange(1,21)
+num=input('Guess a number:')
+if num==randnum:
+    print('You guessed the right number')
+else:
+    print('You guessed the wrong number bitch')
+
+    
