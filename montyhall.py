7618485dad3c49b1fb421ea0f1852a81a57c244b
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-11-20 12:45:22 -0500

    Add files via upload

--- montyhall.py
+++ montyhall.py
@@ -8,6 +8,42 @@ Monty Hall problem
 from random import randint
 car=randint(1,3)
 guess=int(input('Which door would you like to pick:'))
+goat= not car
+if car==1==guess:
+    print('There is a goat behind Door #2')
+    guess2=input('Would you like to change your pick?')
+    if guess2.lower()='yes':
+        print('You won!')
+    else:
+        print('You lost :(')
+elif car==1 and guess==2:
+    print('There is a goat behind Door #3')
+    guess2=input('Would you like to change your pick?')
+    if guess2.lower()='yes':
+        print('You won!')
+    else:
+        print('You lost :(')
+elif car==1 and guess==3:
+    print('There is a goat behind Door #2')
+    guess2=input('Would you like to change your pick?')
+    if guess2.lower()='yes':
+        print('You won!')
+    else:
+        print('You lost :(')
+        
+if car==2 and guess==1:
+    print('There is a goat behind Door #3')
+    guess2=input('Would you like to change your pick?')
+    if guess2.lower()='yes':
+        print('You won!')
+    else:
+        print('You lost :(')
+elif car==2==guess:
+    print('There is a goat behind Door #2')
+    guess2=input('Would you like to change your pick?')
+    if guess2.lower()='yes':
+        print('You won!')
+    else:
+        print('You lost :(')
+elif 
 print('The car was behind Door #', car)
-print('You picked Door#', guess)
-
