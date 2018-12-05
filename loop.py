7dd673ec97e3734fdd5f031827531ce42fd2b026
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-12-05 11:59:08 -0500

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,19 @@
+'''
+Created on Dec 5, 2018
+
+@author: Jennie Mullen
+'''
+num=int(input('Please enter a non-zero number'))
+maxnum=num
+minnum=num
+while num != 0 :
+    if num>maxnum:
+        maxnum=num
+    elif num<minnum:
+        minnum=num
+        
+        
+        
+    
+    
+    
