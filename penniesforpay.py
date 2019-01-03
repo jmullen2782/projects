d00259697a0def75e14a2858c4775b66a227d6d3
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2019-01-02 22:10:44 -0500

    Add files via upload

---
+++ penniesforpay.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 2, 2019
+
+@author: Jennie Mullen
+'''
+days = int(input('Please enter number of days worked: '))
+penny = 1
+print('Day\tSalary')
+print('---------------')
+for salary in range(days):
+  salary=2**(days-1)
+  print(days, salary)
