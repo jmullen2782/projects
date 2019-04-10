ffc40fcc4c5fa5e73426966f75f1889afaceeb61
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2019-04-10 12:38:40 -0400

    Add files via upload

--- encryptedMessage.py
+++ encryptedMessage.py
@@ -4,12 +4,19 @@ Created on Apr 4, 2019
 @author: Jennie Mullen
 '''
 import string
-alphabet = []
+alpha = []
 for c in string.ascii_lowercase:
-    alphabet.append(c)
+    alpha.append(c)
 ralpha = []
-for i in alphabet:
+for i in alpha:
     ralpha.reverse(i)
+shift = 3
 def main():
     text = input("Please enter a file name: ")
+    text.open()
+    text.readline()
+    text.write()
+    for letter in text:
+        
+        
     
