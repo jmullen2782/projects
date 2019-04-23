699f6f959fde605f15c608b0aa9ab2d97220abae
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2019-04-23 12:40:48 -0400

    Add files via upload

--- encryptedMessage.py
+++ encryptedMessage.py
@@ -3,22 +3,32 @@ Created on Apr 4, 2019
 
 @author: Jennie Mullen
 '''
-import string
-alpha = []
-for c in string.ascii_lowercase:
-    alpha.append(c)
-code = []
-count = 0
-for i in range(0,26):
-    code.append((count+i)%26)
-    count+=1
-shift = -3
-alpha.insert(0,alpha[shift:])
-del alpha[shift:]
-text = input("Please enter a file name: ")
-encrypt = open(text,"r")
-
-    
-        
-        
-    
+def cipher():
+    text = input("Please enter a file name: ")
+    encrypt = open(text,"r")
+    new = encrypt.read()
+    new.close()
+    import string
+    alpha = []
+    for c in string.ascii_lowercase:
+        alpha.append(c)
+    shift = int(input("How many places would you like to shift?"))
+    code = []
+    for i in range(0,26):
+        code.append((i+shift)%26)
+    newEncrypt = ''
+    x = 0
+    a = 0
+    for character in encrypt:
+        if encrypt[x] != alpha[a%26]:
+            a += 1
+        elif encrypt[x] = alpha[a]:
+            newEncrypt.append=code[a], sep = ''
+            x += 1
+        elif encrypt[x].isupper():
+            alpha.upper()
+        elif encrypt[x].islower():
+            alpha.lower()
+    print(newEncrypt)
+    test.ENC = open('new','w')
+    print(new)
