6dacb308d0d9fd72f1639be51b622e211f79855f
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2019-05-01 12:41:03 -0400

    Add files via upload

--- encrypMess2.py
+++ encrypMess2.py
@@ -1,33 +1,41 @@
 '''
 Created on Apr 10, 2019
 
-@author: jmullen19
+@author: Jennie Mullen
 '''
-def caesar_cipher(fname, encrypt, shift):
-    fname = input("Please enter a file name: ")
-    file = open(fname,"r")
+def main():
+    text = input('What file would you like to open? ')
+    file = open(text,"r")
     new = file.read()
     new.close()
+    fname = input("Would you like to encrypt or decrypt a file?")
+    shift = int(input("How many places should the alphabet be shifted?"))
+    if fname == "encrypt":
+        encrypt = input('Enter a filename to encrypt: ')
+    else:
+        encrypt = input('Enter a filename to decrypt: ')
+    
+    
+def caesar_cipher(fname, encrypt, shift):
     import string
     alpha = []
     for c in string.ascii_lowercase:
         alpha.append(c)
-    shift = int(input("How many places would you like to shift?"))
-    code = []
-    for i in range(0,26):
-        code.append((i+shift)%26)
+    newalpha = []
+    for i in alpha:
+        newalpha.append((i+shift)%26)
     newEncrypt = ''
     x = 0
     a = 0
-    for character in file[0:]:
-        if file[character] != alpha[a%26]:
+    for character in fname[0:]:
+        if fname[character] != alpha[a%26]:
             a += 1
-        elif file[character] = alpha[a]:
-            newEncrypt.append=code[a], sep = ''
+        elif fname[character] = alpha[a]:
+            newEncrypt.append=newalpha[a], sep = ''
             x += 1
-        elif file[x].isupper():
+        elif fname[x].isupper():
             alpha.upper()
-        elif file[x].islower():
+        elif fname[x].islower():
             alpha.lower()
     print(newEncrypt)
     test.ENC = open('new','w')
