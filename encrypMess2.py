925696537a0f2a229d7b748a419b4382b091e161
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2019-05-03 12:45:32 -0400

    Add files via upload

--- encrypMess2.py
+++ encrypMess2.py
@@ -1,42 +1,54 @@
 '''
 Created on Apr 10, 2019
-
+Encrypted Message 2
 @author: Jennie Mullen
 '''
 def main():
-    text = input('What file would you like to open? ')
-    file = open(text,"r")
-    new = file.read()
-    new.close()
-    fname = input("Would you like to encrypt or decrypt a file?")
+    fname = input("Enter a file to encrypt/decrypt: ")
     shift = int(input("How many places should the alphabet be shifted?"))
-    if fname == "encrypt":
-        encrypt = input('Enter a filename to encrypt: ')
-    else:
-        encrypt = input('Enter a filename to decrypt: ')
-    
-    
+    encrypt = input("Would you like to encrypt or decrypt a file?")
+   
 def caesar_cipher(fname, encrypt, shift):
     import string
     alpha = []
     for c in string.ascii_lowercase:
         alpha.append(c)
     newalpha = []
-    for i in alpha:
-        newalpha.append((i+shift)%26)
-    newEncrypt = ''
-    x = 0
-    a = 0
-    for character in fname[0:]:
-        if fname[character] != alpha[a%26]:
-            a += 1
-        elif fname[character] = alpha[a]:
-            newEncrypt.append=newalpha[a], sep = ''
-            x += 1
-        elif fname[x].isupper():
-            alpha.upper()
-        elif fname[x].islower():
-            alpha.lower()
+    main()
+    if encrypt.lower() == "encrypt":
+        for i in alpha:
+            newalpha.append((i+shift)%26)
+        newEncrypt = ''
+        x = 0
+        a = 0
+        for character in fname[0:]:
+            if fname[character] != alpha[a%26]:
+                a += 1
+            elif fname[character] = alpha[a]:
+                newEncrypt.append=newalpha[a], sep = ''
+                x += 1
+            elif fname[x].isupper():
+                alpha.upper()
+            elif fname[x].islower():
+                alpha.lower()
+    else:
+        shift  *=-1
+        for i in alpha:
+            newalpha.append((i+shift)%26)
+        newEncrypt = ''
+        x = 0
+        a = 0
+        for character in fname[0:]:
+            if fname[character] != alpha[a%26]:
+                a += 1
+            elif fname[character] = alpha[a]:
+                newEncrypt.append=newalpha[a], sep = ''
+                x += 1
+            elif fname[x].isupper():
+                alpha.upper()
+            elif fname[x].islower():
+                alpha.lower()
     print(newEncrypt)
     test.ENC = open('new','w')
     print(new)
+    
